12a4dffec3ab69696ee2f46a55953c68c9f44e06
Urmmine <101092356+Urmmine@users.noreply.github.com>
2022-03-11 21:47:38 +0800

    Add files via upload

---
+++ learn(函数和数组).c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+//函数和数组
+
+////函数
+//Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//
+//	////代码的方式解决
+//	//int sum = num1 + num2;
+//	//printf("%d\n", sum);
+//
+//	//函数的方式解决
+//	int sum = Add(num1, num2);
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
+
+
+////数组 一组相同类型的元素的集合
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	//10个整型1-10存起来
+//	//数组用下标访问(从0开始)
+//	//char ch[5] = { 'a','b','c' };//不完全初始化 剩余的默认为0
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d ", arr[i]);
+//		i++;
+//	}
+//	return 0;
+//}
+
